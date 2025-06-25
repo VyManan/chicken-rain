08629e74dcd1740a713ef0978beebbff6e7ced25
VyManan <kavmanandhar@gmail.com>
2025-06-25 17:01:59 +0900

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+def on_on_chat(_6):
+    blocks.place(GLASS, pos(2, 2, 4))
+player.on_chat("run", on_on_chat)
+
+blocks.place(LAPIS_LAZULI_BLOCK, pos(32, 45, 65))
--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-player.onChat("run", function (_6) {
-    mobs.spawn(TADPOLE, pos(1, 5, 3))
+player.onItemInteracted(IRON_PICKAXE, function () {
+    mobs.spawn(CHICKEN, pos(25, 45, 80))
 })
-mobs.spawn(CHICKEN, pos(10, 4, 7))
+player.onChat("run", function (_6) {
+    blocks.place(GLASS, pos(2, 2, 4))
+})
+player.onChat("jump", function () {
+	
+})
+blocks.place(LAPIS_LAZULI_BLOCK, pos(32, 45, 65))
